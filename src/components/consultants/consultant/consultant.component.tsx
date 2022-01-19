08df7a416efaea5a5b2fc8bc075bdda23bacbb20
Basil Atawneh <basil.atawneh.1998@gmail.com>
2022-01-19 18:29:28 +0200

    Initial commit

---
+++ src/components/consultants/consultant/consultant.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './consultant.module.sass';
+
+const Consultant = (props: any) => {
+  return (
+    <div className={classes.container}>
+      basil
+    </div>
+  )
+}
+
+export default Consultant;
---
+++ src/components/consultants/consultants.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './consultants.module.sass';
+
+const Consultants = (props: any) => {
+  return (
+    <div className={classes.container}>
+      basil
+    </div>
+  )
+}
+
+export default Consultants;
